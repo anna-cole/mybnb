7d3dd5358e8aca36aa270dde33cc9449829da915
Anna Raptidou Cole <acrrj123@gmail.com>
2024-11-14 18:54:24 -0600

    improved responsiveness

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,15 +4,17 @@ const Home = () => {
 
   return (
     <div className="app">
-      <header>
-        <img className="logo" src="/logo_house.png" alt="logo" width="100" height="80"/>
+      <div className="home-container">
+        <div className="home-info">
+          <img className="logo" src="/logo_house.png" alt="logo" width="100" height="80"/>
+          <h1>Welcome to Mybnb!</h1>
+          <h2>Find highly rated property rentals for your next trip!</h2>
+          <Link to="/login">
+            <button type="submit" className="submit-button">Find a property</button>
+          </Link>
+        </div>
         <img className="background-image" src="/camboinhas.png" alt="camboinhas" width="500" height="300"/>
-      </header>
-      <h1>Welcome to Mybnb!</h1>
-      <h2>Find highly rated property rentals for your next trip!</h2>
-      <Link to="/login">
-        <button type="submit" className="submit-button">Find a property</button>
-      </Link>
+      </div>
     </div>
   )
 }
